a8c9c2d8c0348dfe66fdbda8744e4ac43e0c0326
asiermarques <asiermarques@gmail.com>
2021-12-13 22:21:00 +0100

    refactoring

--- src/infrastructure/gcp/cloud_functions/index.ts
+++ src/infrastructure/gcp/cloud_functions/index.ts
@@ -5,25 +5,22 @@ import type {
 } from '@google-cloud/functions-framework/build/src/functions';
 import CommandController from '../../controllers/CommandController';
 import {queryHandler, RouteNotFoundError} from './queryHandler';
-import {fold, tryCatch, Either, chain} from 'fp-ts/lib/Either';
+import {fold, tryCatch, chain} from 'fp-ts/lib/Either';
 import {pipe} from 'fp-ts/function';
 
-function processErrorResponse(e: Error, res: Response) {
+const processErrorResponse = (e: Error, res: Response) => {
   if (e instanceof RouteNotFoundError) {
     res.status(404).send('Not found');
     return;
   }
   res.status(500).send('Internal Error');
-}
+};
 
-function executeController(
-  controller: CommandController
-): Either<Error, string> {
-  return tryCatch<Error, string>(
+const executeController = (controller: CommandController) =>
+  tryCatch<Error, string>(
     () => controller(),
     e => (e instanceof Error ? e : Error('unexpected error'))
   );
-}
 
 export const tripsSlackAppEntrypoint: HttpFunction = (
   req: Request,
--- src/infrastructure/gcp/cloud_functions/queryHandler.ts
+++ src/infrastructure/gcp/cloud_functions/queryHandler.ts
@@ -1,19 +1,16 @@
 import {ParsedQs} from 'qs';
-import CommandController from '../../controllers/CommandController';
 import commandMapping from './query2CommandMap';
-import {Either, left, right} from 'fp-ts/lib/Either';
+import {left, right} from 'fp-ts/lib/Either';
 
 export class RouteNotFoundError extends Error {}
 
 export const COMMAND_PARAM = 'command';
 
-export function queryHandler(
-  query: ParsedQs
-): Either<RouteNotFoundError, CommandController> {
+export const queryHandler = (query: ParsedQs) => {
   const commandInQuery = query[COMMAND_PARAM]?.toString();
   if (commandInQuery) {
     const controller = commandMapping.get(commandInQuery);
     if (controller !== undefined) return right(controller);
   }
   return left(new RouteNotFoundError());
-}
+};
